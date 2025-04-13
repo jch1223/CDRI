37effc64b951ae9a5e515359487968a38cec5f8b
jch1223 <ico1828@gmail.com>
2025-04-14 00:59:26 +0900

    icon에 width, height, color props 추가

--- src/components/Icons/FillLike.tsx
+++ src/components/Icons/FillLike.tsx
@@ -1,16 +1,24 @@
-export const FillLikeIcon = () => {
+export const FillLikeIcon = ({
+  width = 24,
+  height = 24,
+  color = '#E84118',
+}: {
+  width?: number;
+  height?: number;
+  color?: string;
+}) => {
   return (
     <svg
-      width="24"
-      height="24"
+      width={width}
+      height={height}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         d="M7.5 4C4.46244 4 2 6.46245 2 9.5C2 15 8.5 20 12 21.1631C15.5 20 22 15 22 9.5C22 6.46245 19.5375 4 16.5 4C14.6398 4 12.9953 4.92345 12 6.3369C11.0047 4.92345 9.36015 4 7.5 4Z"
-        fill="#E84118"
-        stroke="#E84118"
+        fill={color}
+        stroke={color}
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
